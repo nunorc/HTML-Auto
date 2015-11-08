8b1b0811123ce1e8365790c90bfee95de929a373
Nuno Carvalho <mestre.smash@gmail.com>
2015-11-08 17:35:57 +0000

    Release 0.05

--- lib/HTML/Auto.pm
+++ lib/HTML/Auto.pm
@@ -19,11 +19,11 @@ HTML::Auto - automatic write HTML for common elements
 
 =head1 VERSION
 
-Version 0.04
+Version 0.05
 
 =cut
 
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 
 =head1 SYNOPSIS
